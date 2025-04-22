a4306c672c174f63353274cfd3b490184701a210
Hacks-coder <ajheartcry@gmail.com>
2025-04-21 17:04:55 -0700

    second commiting

--- app/src/components/Main/Hero.svelte
+++ app/src/components/Main/Hero.svelte
@@ -2,8 +2,8 @@
 	import Fill from "../UI/Buttons/Fill.svelte";
     const hero = "./hero/img1.webp"
 </script>
-<section style="background-image: url({hero});" class="@container  bg-cover bg-center flex flex-col gap-5 items-center size-full text-center text-white py-10 px-5 @xl:gap-7 @xl:p-30 @3xl:gap-15 @3xl:p-50">
-    <p class="text-2xl font-bold @xl:text-4xl @3xl:text-6xl">The Next Phase of Gaming...</p>
+<section style="background-image: url({hero});" class="@container  bg-cover bg-center flex flex-col gap-5 items-center size-full text-center text-white py-10 px-5 p-20 @xl:gap-7@xl:p-40  @3xl:gap-15 @3xl:p-50">
+    <p class="text-3xl font-bold @xl:text-5xl @3xl:text-7xl">The Next Phase of Gaming...</p>
     <p>Get up to $300 Samsung Credit <sup>24</sup>and more when you pre-order.</p>
     <Fill text = "Pre-order now!" />
     <a href="/" class="underline">Shop all monitor deals</a>
--- app/src/components/UI/Buttons/Fill.svelte
+++ app/src/components/UI/Buttons/Fill.svelte
@@ -1,5 +1,5 @@
 <script>
-    let {text, className = "bg-white w-fit shadow-lg shadow-gray-400 rounded-full px-7 h-10 @xl:h-15"} = $props();
+    let {text, className = "bg-transparent border-2 border-white w-fit shadow-lg shadow-gray-400 rounded-full px-7 h-10 @xl:h-15"} = $props();
 </script>
 
 <button class={className}>{text}</button>
